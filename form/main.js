08dc306949396713174615e9f1701045b42a4fa4
leosfalsim <lnsfalsim@gmail.com>
2023-02-14 12:47:02 -0300

    Corrige id do botao

--- form/main.js
+++ form/main.js
@@ -1,6 +1,6 @@
-const signUpButton = document.getElementById('create');
-const signInButton = document.getElementById('entrar');
+const createButton = document.getElementById('create');
+const logInButton = document.getElementById('logIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', ()=> container.classList.add('right-panel-active'));
-signInButton.addEventListener('click', ()=> container.classList.remove('right-panel-active'));
+createButton.addEventListener('click', ()=> container.classList.add('right-panel-active'));
+logInButton.addEventListener('click', ()=> container.classList.remove('right-panel-active'));
